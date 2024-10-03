7d412b12669d2bce4135d29ed5038efcfc198db7
wasabina67 <wasabina67@gmail.com>
2024-10-03 21:13:19 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,31 @@
   <div>
     <h1>やさシェフ / プロンプト作成補助</h1>
     <p>WIP</p>
+    <div v-if="generatedPrompt">
+      <h4>プロンプト</h4>
+      <textarea v-model="generatedPrompt" readonly></textarea>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'App',
-  components: {
+  data() {
+    return {
+      item: {
+        ingredients: '',
+      },
+      generatedPrompt: 'aaaaa'
+    }
+  },
+  methods: {
+    generate() {
+      this.generatedPrompt = `aaaaa`
+    },
+    clear() {
+      this.generatedPrompt = ''
+    }
   }
 }
 </script>
@@ -21,5 +39,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+textarea {
+  resize: vertical;
+  width: 600px;
+  height: 150px;
 }
 </style>
