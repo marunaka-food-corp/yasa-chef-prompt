9988e8ae353fb299a42c36ff626905ffa4005261
wasabina67 <wasabina67@gmail.com>
2024-10-03 21:35:37 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>やさシェフ / プロンプト作成補助</h1>
     <form @submit.prevent="generate">
-      <button type="submit">作成</button>
+      <button type="submit">プロンプト作成</button>
     </form>
     <div v-if="generatedPrompt" class="generated-prompt">
       <textarea v-model="generatedPrompt" readonly></textarea>
@@ -39,9 +39,10 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
   margin-top: 60px;
+  margin-left: 60px;
 }
 
 .generated-prompt {
