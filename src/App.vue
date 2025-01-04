bec82f44463b23c227a1c1ef00953826194b6480
wasabina67 <wasabina67@gmail.com>
2025-01-04 20:40:01 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -55,8 +55,6 @@ ${ingredients}`
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: left;
   color: #2c3e50;
   margin-top: 30px;
